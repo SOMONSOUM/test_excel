27ab3948d9aec3d3e28eca5372673fd97b414c22
SOMON SOUM <somon.soum9@gmail.com>
2023-08-07 22:10:11 +0700

    fix

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const XLSX = require("xlsx");
 
-// Read the Excel file
 const workbook = XLSX.readFile("export_sheet.xlsx");
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-// Convert worksheet to JSON
 const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-// Get the column index of 'hscode'
+//Column's name
 const columnIndex = jsonData[0].indexOf("HS CODE 2022");
-console.log(columnIndex);
 
-// Modify the data in the 'hscode' column
 for (let i = 1; i < jsonData.length; i++) {
   const value = jsonData[i][columnIndex];
   if (typeof value === "string" && value.includes(",")) {
@@ -22,11 +18,7 @@ for (let i = 1; i < jsonData.length; i++) {
   }
 }
 
-// Convert JSON back to worksheet
 const updatedWorksheet = XLSX.utils.json_to_sheet(jsonData);
-
-// Update the original workbook with the updated worksheet
 workbook.Sheets[workbook.SheetNames[0]] = updatedWorksheet;
 
-// Save the modified workbook to a new file
 XLSX.writeFile(workbook, "./output/export_sheet_output.xlsx");
